fe1e3f3091608cc755637bd8e163805e7c942f4b
Henson Dannhorn <hdannhorn3645@student.pps.net>
2025-12-04 15:34:07 -0800

    merge

--- Scripts/camera_2d.gd
+++ Scripts/camera_2d.gd
@@ -2,8 +2,8 @@ extends Camera2D
 
 @onready var player = %Player
 
-const FOLLOW_SPEED = 0.3
+const FOLLOW_SPEED = 500
 
 
 func _physics_process(delta):
-	position = position.move_toward(player.position, position.distance_to(player.position) * FOLLOW_SPEED)
+	self.position = self.position.move_toward(player.position, self.position.distance_to(player.position) * FOLLOW_SPEED)
