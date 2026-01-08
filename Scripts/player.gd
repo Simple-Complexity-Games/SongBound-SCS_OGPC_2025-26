b020a1ce1d25b077e1601eb8748a6870570ea0f3
Henson Dannhorn <hdannhorn3645@student.pps.net>
2026-01-08 14:50:46 -0800

    Developer tools quick movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -51,8 +51,17 @@ func _process(_delta: float) -> void:
 	Handle_Fullscreening()
 	
 	#Developer Tools
-	if Input.is_action_just_pressed("Teleport"):
-		position.x += 2000
+	if Input.is_action_just_pressed("Teleport_Up"):
+		position.y -= 200
+		
+	if Input.is_action_just_pressed("Teleport_Down"):
+		position.y += 200
+		
+	if Input.is_action_just_pressed("Teleport_Left"):
+		position.x -= 1500
+		
+	if Input.is_action_just_pressed("Teleport_Right"):
+		position.x += 1500
 
 func _physics_process(delta: float) -> void:
 	#Apply gravity
