eb4dfbf3f48ede5957bc176d9a81be6a2e7faacb
Emmet Navarra <enavarra3973@student.pps.net>
2026-01-16 15:30:44 -0800

    added the dammage and heal functions

--- Scripts/health_manager.gd
+++ Scripts/health_manager.gd
@@ -1,8 +1,17 @@
 extends Node2D
 
 @export var max_health = 0
+
 var current_health = 0
+
+var taking_damage = false
 
 func _ready() -> void:
 	current_health = max_health
 	
+func Damage(value, ignore_i_frames = false):
+	taking_damage = true
+	current_health -= value
+	
+func Heal(value, ignore_max_health = false):
+	current_health += value
