0ab6dfe43b1d0c273de46752b119b66b35e78049
Emmet Navarra <enavarra3973@student.pps.net>
2026-01-29 15:20:18 -0800

    added water to a map and changed camera limits

--- Scripts/camera_2d.gd
+++ Scripts/camera_2d.gd
@@ -2,13 +2,6 @@ extends Camera2D
 
 @onready var player = %Player
 const FOLLOW_SPEED = 100
-
-@export var left = -10000000
-@export var right = 10000000
-
-func _ready() -> void:
-	limit_left = left
-	limit_right = right
 	
 func _physics_process(_delta):
 	self.position = self.position.move_toward(player.position, self.position.distance_to(player.position) * FOLLOW_SPEED)
