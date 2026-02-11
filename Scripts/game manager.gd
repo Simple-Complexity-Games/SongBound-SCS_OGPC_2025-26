ba67a9faedef47109c80ef54956b738e66f9714c
Emmet Navarra <enavarra3973@student.pps.net>
2026-02-11 15:17:46 -0800

    to small to specify\

--- Scripts/game manager.gd
+++ Scripts/game manager.gd
@@ -10,3 +10,8 @@ func new_player_location():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+# make the sign script on ready to signal the game manager to teleport the player.
+# make it where when the player enters the zone of the sign scripts it records the 
+#name of the sign or a value you give it, from this when a new scene is loaded the sign would 
+#signal the global scipt which would teleport the player to a sign in the new scene 
+#with the same value as the sign in the previous scene.
