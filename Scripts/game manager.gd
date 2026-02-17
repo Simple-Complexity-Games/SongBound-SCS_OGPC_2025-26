80b8ec6f46b0b1917d177500964e7eeae7f40c7f
Emmet Navarra <enavarra3973@student.pps.net>
2026-02-17 15:23:47 -0800

    player position in new scene almost completed

--- Scripts/game manager.gd
+++ Scripts/game manager.gd
@@ -4,10 +4,10 @@ var sign_location = "hi"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
+	
 func new_player_location():
 	pass
-
+	#get_tree().call_group("Signs",sign_text = sign_location).global_position
 
 # make the sign script on ready to signal the game manager to teleport the player.
 # make it where when the player enters the zone of the sign scripts it records the 
