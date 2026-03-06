56166769a1f13ccc8001c51623e5f629e522d078
Emmet Navarra <enavarra3973@student.pps.net>
2026-03-06 15:23:52 -0800

    one hit box of the wapon works

--- Scripts/Weapon.gd
+++ Scripts/Weapon.gd
@@ -2,15 +2,16 @@ extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	position.y = -12
 
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_pressed("Attack") and Input.is_action_pressed("Right"):
+		position.x = 67.5
+		$attack_box/CollisionShape2D.set_deferred("disabled", false)
+		await get_tree().create_timer(1).timeout
+		$attack_box/CollisionShape2D.set_deferred("disabled", true)
+		position.x = 0
 
-func Attack():
-	if Input.is_action_pressed("Attack"):
-		
-		if Input.is_action_pressed("Right"):
-			$attack_box.position.x = 67.5
-			$attack_box/CollisionShape2D.disabled = false
-			get_tree().create_timer(1)
-			$attack_box/CollisionShape2D.disabled = true
-# 80 pixels form player for the middle hit boxes right side to the players right side. each box is 24 wide, 8 tall.
+# 80 pixels form player for the middle hit boxes right side to the players right side. each box is 25 wide, 8 tall.
 #think of its middle of 12 when possitioning as in minus it by 12 dumbass
