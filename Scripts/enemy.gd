6744cbd9f31748b9c0c010892c911030406b4451
Zan456-Coding <Ahedberg4554@student.pps.net>
2026-03-12 14:11:03 -0700

    Aggro timer is working

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,18 +1,20 @@
 extends CharacterBody2D
 
 var Direction = 1
-var Alert = false
 var Player_Position
-var Enemy_Position = self.position.x
-#var Distance = Enemy_Position - Player_Position
+var Enemy_Position = self.position.x #Getting enemy position
 var Speed = 50
+#var Idle = false #Agrro timer went off, Alert won't be true till player renters the enemy's sphere
+var Alert = false #Becomes true once the player comes into the sphere. Will chase the player when true
+var Agrro_Timer = 0  #Timer for how long the enemy will chase the player till it gives up and stops.
+var Chase = false
 
 @onready var ray_right: RayCast2D = $RayRight
 @onready var ray_left: RayCast2D = $RayLeft
 
 #EnemyScountingMovement
 func _physics_process(delta: float) -> void:
-	velocity += get_gravity() * delta
+	velocity += get_gravity() * delta 
 
 	Player_Position = get_parent().get_node("Player").position.x
 	Enemy_Position = self.position.x
@@ -23,23 +25,31 @@ func _physics_process(delta: float) -> void:
 		Direction = 1
 	
 	
-	if Alert == false: #Patroling when player is out of view
+	if !Alert: #Patroling when player is out of view
 		position.x += Direction * Speed * delta
 	
-	if Alert: #Chasing player when in view
-	
+	if Alert: #Chasing player whenever Alert or Chase equals true
 		if Enemy_Position - Player_Position < 0: #Positive
 			velocity.x = move_toward(velocity.x, Speed, 5)
 		if Enemy_Position - Player_Position > 0: #Negative
 			velocity.x = move_toward(velocity.x, -Speed, 5)
-			
-	
-	
-	#position.x = move_toward(position.x, Enemy_Position.x, Speed)
 	
 	move_and_slide()
 
+
 func _on_alerting_area_body_entered(_body: Node2D) -> void: #Player is in view
 	Alert = true
+	Chase = false
 func _on_alerting_area_body_exited(_body: Node2D) -> void: #Player is not in view
-	Alert = false
+	$AggroTimer.start()
+	Chase = true
+	Agrro_Timer = 0 #Restarting the Aggro_Timer if not in range
+	print(Agrro_Timer)
+
+func _on_timer_timeout() -> void: #three second timer
+	if Agrro_Timer <= 9 and Chase == true: #Plusing it once each time the timer goes off
+		Agrro_Timer += 1
+		$AggroTimer.start()
+		print(Agrro_Timer)
+	if Agrro_Timer == 10: #If the Agrro_Timer reaches 10 than we reset the timer and unalert the enemy
+		Alert = false
