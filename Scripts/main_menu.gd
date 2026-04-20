b3ac5f0508593955df4b4a476c09973d598847c7
Emmet Navarra <enavarra3973@student.pps.net>
2026-04-20 15:16:54 -0700

    cant jump out of owl house

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -937,6 +937,8 @@ func Save_Window_Position(x = null, y = null):
 
 #region ------Navigation Functions------
 func Start_Game():
+	GameManager.pos_x = 650
+	GameManager.pos_y = 22
 	get_tree().change_scene_to_file("res://Scenes/Areas/Sparrow_sanctuary/path_one.tscn")
 
 func Load_Main_Menu():
